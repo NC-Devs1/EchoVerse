2e4835eccda9d5122b42bf51a650cdbd5558236b
Neeka Chisholm <c.neek627@gmail.com>
2025-02-08 12:10:49 -0500

    Initial Commit

---
+++ EchoVerse/EchoVerseApp.swift
@@ -0,0 +1,17 @@
+//
+//  EchoVerseApp.swift
+//  EchoVerse
+//
+//  Created by Neeka Chisholm on 2/8/25.
+//
+
+import SwiftUI
+
+@main
+struct EchoVerseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ EchoVerseTests/EchoVerseTests.swift
@@ -0,0 +1,17 @@
+//
+//  EchoVerseTests.swift
+//  EchoVerseTests
+//
+//  Created by Neeka Chisholm on 2/8/25.
+//
+
+import Testing
+@testable import EchoVerse
+
+struct EchoVerseTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
